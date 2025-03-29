1c8933834f3c84d4aa903f2f67cdbd8e94ba5459
hamzahbondok <hamzah.bondok@gmail.com>
2025-03-28 22:57:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -193,5 +193,6 @@ if [ -f /etc/systemd/logind.conf.d/logind.conf ]; then
 else
     sudo mkdir /etc/systemd/logind.conf.d
     sudo touch /etc/systemd/logind.conf.d/logind.conf
+    echo "[Login]" | sudo tee -a /etc/systemd/logind.conf.d/logind.conf
     echo "HandlePowerKey=sleep" | sudo tee -a /etc/systemd/logind.conf.d/logind.conf
     echo "KillUserProcesses=True" | sudo tee -a /etc/systemd/logind.conf.d/logind.conf
